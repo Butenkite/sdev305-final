62c93476beea2643511b39e0450ed16fcd1c9cfe
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-03-13 14:16:12 -0700

    finished

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 
+const bookArray = [];
+
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -7,8 +9,26 @@ app.use(express.static('public'));
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-res.render('home');
+    res.render('home');
 });
+
+app.post('/summary', (req, res) => {
+
+    const bookobject = {
+        title: req.body.title,
+        comments: req.body.comments,
+        rating: req.body.rating
+    }
+
+    console.log(bookobject);
+
+    bookArray.push(bookobject);
+
+    console.log(bookArray);
+
+    res.render('summary', {bookArray});
+    });
+    
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
